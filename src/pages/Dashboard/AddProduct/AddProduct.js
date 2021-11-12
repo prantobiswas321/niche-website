5b245a2f83251ac4aeaee332afa0b4c5a84b1aaf
prantobiswas321 <rahulbd321@gmail.com>
2021-11-12 20:03:13 +0600

    add product done

--- src/pages/Dashboard/AddProduct/AddProduct.js
+++ src/pages/Dashboard/AddProduct/AddProduct.js
@@ -1,9 +1,40 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useState } from 'react';
+import { useForm } from "react-hook-form";
+import './AddProduct.css';
 
 const AddProduct = () => {
+    const { register, handleSubmit, reset } = useForm();
+    const [addSuccess, setAddSuccess] = useState(false);
+
+    const onSubmit = data => {
+        console.log(data);
+
+        axios.post('http://localhost:5000/products', data)
+            .then(res => {
+                if (res.data.insertedId) {
+                    setAddSuccess(true);
+                    reset();
+                }
+            })
+    }
     return (
-        <div>
-            <h2>Add Product</h2>
+        <div className='add-service mt-5 mb-5'>
+            <h1 className='mb-5'>Please add a place</h1>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <input {...register("name", { required: true, maxLength: 20 })} placeholder='Name' />
+                <input type="number" {...register("price")} placeholder='Price' />
+                <input {...register("image")} placeholder='Image url' />
+                <textarea {...register("description")} placeholder='Description' />
+                {/* <input type="submit" /> */}
+                <button className='btn btn-success'>Submit</button>
+                {
+                    addSuccess && <div className="alert alert-success mt-3" role="alert">
+                        Product added Successfully!
+                    </div>
+                }
+            </form>
+
         </div>
     );
 };
