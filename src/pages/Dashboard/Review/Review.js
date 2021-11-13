7dc1275a24623aa406b322f78a29e2206de8b273
prantobiswas321 <rahulbd321@gmail.com>
2021-11-13 12:52:52 +0600

    add review done

--- src/pages/Dashboard/Review/Review.js
+++ src/pages/Dashboard/Review/Review.js
@@ -1,9 +1,83 @@
-import React from 'react';
+import { useRef, useState } from 'react';
+import useAuth from '../../../hooks/useAuth';
+import review from '../../../images/review.png';
+import './Review.css';
 
 const Review = () => {
+    const { user } = useAuth();
+    const [reviewSuccess, setReviewSuccess] = useState(false);
+    const ratingRef = useRef();
+    const reviewRef = useRef();
+
+    const addReview = e => {
+        const review = {
+            displayName: user.displayName,
+            email: user.email,
+            rating: ratingRef.current.value,
+            comment: reviewRef.current.value
+        };
+        fetch('http://localhost:5000/addReview', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(review)
+        })
+            .then(res => res.json())
+            .then(data => {
+                setReviewSuccess(true);
+                e.target.reset();
+            })
+        e.preventDefault();
+    }
+
     return (
         <div>
-            <h2>This is review</h2>
+            <h2>Give Your Feedback</h2>
+            <div className="container mt-5">
+                <div className="row g-3 justify-content-md-center">
+                    <div className="col-12 col-lg-5">
+                        <img className="img-fluid" src={review} alt="" />
+                    </div>
+                    <div className="my-auto col-12 col-lg-7">
+                        <form onSubmit={addReview} className='mt-3'>
+                            <div className="row mb-3">
+                                <label className="col-sm-2 col-form-label">Name :</label>
+                                <div className="col-sm-10">
+                                    <input type="text" className="form-control" defaultValue={user?.displayName} name="name" required disabled />
+                                </div>
+                            </div>
+                            <div className="row mb-3">
+                                <label className="col-sm-2 col-form-label">Email :</label>
+                                <div className="col-sm-10">
+                                    <input type="email" className="form-control" name="email"
+                                        defaultValue={user?.email} required disabled />
+                                </div>
+                            </div>
+
+                            <div className="row mb-3">
+                                <label className="col-sm-2 col-form-label">Rating: </label>
+                                <div className="col-sm-10">
+                                    <input type="number" min="0" max="5" className="form-control" ref={ratingRef} name="rating" required />
+                                </div>
+                            </div>
+
+                            <div className="row mb-3">
+                                <label className="col-sm-2 col-form-label">Comment :</label>
+                                <div className="col-sm-10">
+                                    <textarea className="w-100" ref={reviewRef}></textarea>
+                                </div>
+                            </div>
+                            <input type="submit" value="Submit" className="bg-success px-3 py-2 rounded-3 text-white" />
+                            {
+                                reviewSuccess && <div className="alert alert-success mt-3" role="alert">
+                                    Review added Successfully!
+                                </div>
+                            }
+                        </form>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
